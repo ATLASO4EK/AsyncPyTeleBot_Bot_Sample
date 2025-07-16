fffed81b06c95768d5fdf411a00981150cefb181
ATLASO4EK <atlaso4ek@gmail.com>
2025-07-16 18:09:48 +0300

    bot structure init

--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -1,1 +1,10 @@
 import logging
+from states import *
+from bot import *
+
+#Реакция бота на команду /start
+@bot.message_handler(commands=['start'])
+async def start(message, state: StateContext):
+    logging.info(msg=f'/start - user:{message.from_user.id}')
+    text = 'Привет!\nЯ - чат-бот по мотивации вашей работы!\nПожалуйста, введите ваш логин'
+    await bot.send_message(message.chat.id, text)
--- src/bot/main.py
+++ src/bot/main.py
@@ -3,18 +3,19 @@ import logging
 from telebot import asyncio_filters
 from telebot.states.asyncio.middleware import StateMiddleware
 
-from bot import *
 from handlers import *
 
 logging.basicConfig(level=logging.INFO,
                     filename="log.log",
-                    filemode="w",
+                    filemode="a",
                     format="%(asctime)s %(levelname)s %(message)s")
 
 bot.add_custom_filter(asyncio_filters.StateFilter(bot))
 bot.setup_middleware(StateMiddleware(bot))
 try:
+    print('bot started')
     logging.info('bot started')
     asyncio.run(bot.polling())
 except Exception as e:
+    print(f'bot crashed with exception:\n{e}')
     logging.error(f'bot crashed with exception:\n{e}')
