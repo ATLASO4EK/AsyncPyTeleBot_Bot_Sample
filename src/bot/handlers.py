d173b40794c92b5dbd2268da37d23755dfcf35d1
ATLASO4EK <atlaso4ek@gmail.com>
2025-07-16 18:32:56 +0300

    bot structure init

--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -5,6 +5,7 @@ from bot import *
 #Реакция бота на команду /start
 @bot.message_handler(commands=['start'])
 async def start(message, state: StateContext):
-    logging.info(msg=f'/start - user:{message.from_user.id}')
-    text = 'Привет!\nЯ - чат-бот по мотивации вашей работы!\nПожалуйста, введите ваш логин'
-    await bot.send_message(message.chat.id, text)
+    try:
+        await bot.send_message(message.chat.id, 'Привет!\nЯ - чат-бот по мотивации вашей работы!\nПожалуйста, введите ваш логин')
+    except Exception as e:
+        logging.error(f'/start user:{message.from_user.id} ')
